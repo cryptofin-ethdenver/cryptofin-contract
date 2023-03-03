310c0c58f611355566e5bea1b4f2710ce008899e
ArtForyou <hydrun3@gmail.com>
2023-03-04 03:08:29 +0900

    Create Truffle&Infura configurations for multichain-deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,12 @@
-const dotenv = require('dotenv');
-dotenv.config();
-
 const FaucetToken = artifacts.require("FaucetToken");
 
 module.exports = function (deployer) {
     deployer.deploy(FaucetToken, "USD Coin", "USDC", 6);
-    // deployer.deploy(FaucetToken, "Tether USD", "USDT");
-    // deployer.deploy(FaucetToken, "Matic Token", "MATIC");
-    // deployer.deploy(FaucetToken, "Oasis Network", "ROSE");
+    deployer.deploy(FaucetToken, "Tether USD", "USDT", 6);
+
+    /* Except for Mumbai */
+    // deployer.deploy(FaucetToken, "Matic Token", "MATIC", 18);
+
+    /* For Polygon Mumbai */
+    // deployer.deploy(FaucetToken, "Ethereum", "ETH", 18);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const privateKey = process.env.DEPLOYER_WALLET_PRIVATEKEY;
-const goerliNode = process.env.GOERLI_TESTNET_NODE_URI;
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
 
 module.exports = {
   networks: {
@@ -14,11 +14,51 @@ module.exports = {
       network_id: "5777"
     },
     goerli: {
-      provider: () => new HDWalletProvider(privateKey, goerliNode),
+      provider: () => new HDWalletProvider(privateKey, `https://goerli.infura.io/v3/${INFURA_API_KEY}`),
       network_id: '5',
       gas: '800000',
-      gasPrice: null
+      gasPrice: null,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
     },
+    mumbai: {
+      provider: () => new HDWalletProvider(privateKey, `https://polygon-mumbai.infura.io/v3/${INFURA_API_KEY}`),
+      network_id: 80001,
+      gas: '800000',
+      gasPrice: null,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    aurora: {
+      provider: () => new HDWalletProvider(privateKey, `https://aurora-testnet.infura.io/v3/${INFURA_API_KEY}`),
+      network_id: 1313161555,
+      gas: '800000',
+      gasPrice: null,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+    },
+    base: {
+      provider: () => new HDWalletProvider(privateKey, `https://goerli.base.org/`),
+      network_id: 84531,
+      gas: '800000',
+      gasPrice: null,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    scroll: {
+      provider: () => new HDWalletProvider(privateKey, `https://alpha-rpc.scroll.io/l2`),
+      network_id: 534353,
+      gas: '800000',
+      gasPrice: null,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+
   },
 
   mocha: {
